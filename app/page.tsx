c1b013733c83e5235828a81b34691478c8dc88f7
val-- <khayvav@gmail.com>
2024-06-23 17:49:25 +0200

    shuffle questions

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@ import data from './sample.json';
 import data2 from './sample2.json';
 
 import Quiz from '../components/quiz';
-import { Quiz as QuizType } from './types';
+import { Question } from './types';
 
 export default function Home() {
   const quiz = (Math.random()>=0.5)? data : data2
+  const shuffle = (array: Question[]) => { 
+    return array.sort(() => Math.random() - 0.5); 
+};
   return (
     <div className="home-container flex items-center justify-center min-h-screen bg-black">
-      <Quiz title={quiz.thème} questions={quiz.quizz.débutant } />
+      <Quiz title={quiz.thème} questions={shuffle(quiz.quizz.débutant) } />
     </div>
   );
 }
