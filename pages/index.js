71e64fa2a3e203b2675f66092d36a0e3a3afff04
val-- <khayvav@gmail.com>
2024-10-06 10:07:58 +0200

    initialize project

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to this Quiz project</h1>
+        <p>This is running inside a dev container!</p>
+      </div>
+    );
+  }
