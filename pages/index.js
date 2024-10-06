4210d6d65b0032c2594d1755f439d705be8dbab9
val-- <khayvav@gmail.com>
2024-10-06 10:21:38 +0200

    instead of of pages router, use app router

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Welcome to this Quiz project</h1>
-        <p>This is running inside a dev container!</p>
-      </div>
-    );
-  }
