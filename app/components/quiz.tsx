343d307c3b7d4a6ae76411a0eb3c34c3cef0ec99
val-- <khayvav@gmail.com>
2024-10-06 13:38:12 +0200

    Text and margin reduction to fit on small screen

--- app/components/quiz.tsx
+++ app/components/quiz.tsx
@@ -28,15 +28,15 @@ const Quiz: React.FC<QuizProps> = ({ title, questions }) => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 via-purple-600 to-pink-500">
-      <div className="container max-w-2xl bg-white bg-opacity-80 shadow-lg rounded-lg p-8">
-        <h2 className="text-3xl font-bold mb-6 text-center text-purple-800">{title}</h2>
+      <div className="container max-w-2xl bg-white bg-opacity-80 shadow-lg rounded-lg p-6">
+        <h2 className="text-2xl font-bold mb-4 text-center text-purple-800">{title}</h2>
         {!showResult ? (
           <div>
             <Question
               question={questions[currentQuestionIndex]}
               handleAnswer={handleAnswer}
             />
-            <div className="flex justify-between mt-8 text-lg text-gray-700">
+            <div className="flex justify-between mt-6 text-lg text-gray-700">
               <p>Votre score: <strong>{score} point(s)</strong></p>
               <p>Question {currentQuestionIndex + 1} / {questions.length}</p>
             </div>
