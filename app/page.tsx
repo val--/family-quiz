cab37424d02af1b4074a159f268aeb0af5e04ade
val-- <khayvav@gmail.com>
2024-10-06 19:37:27 +0200

    Variabilize the app name

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ interface FamilyMember {
   name: string;
   email: string;
   photo: string;
-  lastScore?: number; // Optional because it might not exist initially
+  lastScore?: number;
 }
 
 const familyMembers: FamilyMember[] = [
@@ -43,7 +43,7 @@ const getLastScores = () => {
     const lastScore = localStorage.getItem(`score_${member.id}`);
     return {
       ...member,
-      lastScore: lastScore ? parseInt(lastScore, 10) : null, // Set null instead of 0 if no score
+      lastScore: lastScore ? parseInt(lastScore, 10) : null,
     };
   });
 };
@@ -62,11 +62,11 @@ export default function Home() {
     }, membersWithScores[0]);
 
     setHighestScorerId(highestScorer.id);
-    setLoading(false);
+    setLoading(false); // Stop loading once data is fetched
   }, []);
 
-  const handleStartQuiz = (member: FamilyMember) => {   // = save the selected user in localStorage and navigate to the quiz page
-    localStorage.setItem("currentUser", JSON.stringify(member));
+  const handleStartQuiz = (member: FamilyMember) => {
+    localStorage.setItem("currentUser", JSON.stringify(member)); // Store selected user in localStorage
   };
 
   if (loading) {
@@ -76,8 +76,12 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 p-2">
       <div className="text-center">
-        <h1 className="text-white text-5xl font-bold mb-8">Bienvenue sur le Guiberre Quiz <span>🐱</span></h1>
-        <p className="text-white text-xl mb-6">Choisis un membre de la famille pour commencer le quiz ! Qui es-tu ?</p>
+        <h1 className="text-white text-5xl font-bold mb-8">
+          Bienvenue sur le {process.env.NEXT_PUBLIC_APP_NAME || "Family"} Quiz <span>🐱</span>
+        </h1>
+        <p className="text-white text-xl mb-6">
+          Choisis un membre de la famille pour commencer le quiz ! Qui es-tu ?
+        </p>
 
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6 place-items-center">
           {members.map((member) => (
@@ -91,6 +95,9 @@ export default function Home() {
                 src={member.photo}
                 alt={`Photo de ${member.name}`}
                 className="w-28 h-28 rounded-full mb-2 object-cover"
+                onError={(e) => {
+                  e.currentTarget.src = "/images/default-avatar.png";
+                }}
               />
               <span className="z-10 text-xl">
                 {member.name}
