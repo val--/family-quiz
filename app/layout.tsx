35088c4e1effb5584d1d7a9a529a4a50e629dc12
val-- <khayvav@gmail.com>
2025-01-26 11:08:04 +0100

    dont make layout.tsx a client component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
-"use client";
-
 import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
+import SessionProviderWrapper from "app/components/SessionProviderWrapper";
 import Link from "next/link";
 import AuthButtons from "app/components/AuthButtons";
+import { getServerSession } from "next-auth";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+
+  const session = await getServerSession();
+
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500">
-        <SessionProvider>
+        <SessionProviderWrapper session={session}>
           <header className="w-full py-2 bg-opacity-50 bg-black text-white text-center shadow-md">
             <div className="container mx-auto flex justify-between items-center px-4">
               <Link href="/" className="text-white text-lg font-semibold hover:text-gray-300 transition">
@@ -19,7 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </div>
           </header>
           <main className="flex-grow">{children}</main>
-        </SessionProvider>
+        </SessionProviderWrapper>
       </body>
     </html>
   );
